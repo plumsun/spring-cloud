2d92030b2ac5941d0f33ddaa2a54bead555a749b
lihaohan <plumsun@163.com>
2023-12-12 21:05:53 +0800

    代码格式优化

--- cloud-demo/config-server/src/main/java/com/itheima/config/UrlFilter.java
+++ cloud-demo/config-server/src/main/java/com/itheima/config/UrlFilter.java
@@ -26,23 +26,23 @@ public class UrlFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest httpServletRequest = (HttpServletRequest)request;
-        HttpServletResponse httpServletResponse = (HttpServletResponse)response;
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) response;
 
         String url = new String(httpServletRequest.getRequestURI());
 
-        //只过滤/actuator/bus-refresh请求
+        // 只过滤/actuator/bus-refresh请求
         if (!url.endsWith("/bus-refresh")) {
             chain.doFilter(request, response);
             return;
         }
 
-        //获取原始的body
+        // 获取原始的body
         String body = readAsChars(httpServletRequest);
 
-        System.out.println("original body:   "+ body);
+        System.out.println("original body:   " + body);
 
-        //使用HttpServletRequest包装原始请求达到修改post请求中body内容的目的
+        // 使用HttpServletRequest包装原始请求达到修改post请求中body内容的目的
         CustometRequestWrapper requestWrapper = new CustometRequestWrapper(httpServletRequest);
 
         chain.doFilter(requestWrapper, response);
@@ -88,35 +88,24 @@ public class UrlFilter implements Filter {
         }
     }
 
-    public static String readAsChars(HttpServletRequest request)
-    {
+    public static String readAsChars(HttpServletRequest request) {
 
         BufferedReader br = null;
         StringBuilder sb = new StringBuilder("");
-        try
-        {
+        try {
             br = request.getReader();
             String str;
-            while ((str = br.readLine()) != null)
-            {
+            while ((str = br.readLine()) != null) {
                 sb.append(str);
             }
             br.close();
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
-        }
-        finally
-        {
-            if (null != br)
-            {
-                try
-                {
+        } finally {
+            if (null != br) {
+                try {
                     br.close();
-                }
-                catch (IOException e)
-                {
+                } catch (IOException e) {
                     e.printStackTrace();
                 }
             }
